9d53926f1ec415af90edcf198a8671ede315b79c
MarvinDev911 <mar.godtzy@gmail.com>
2024-03-20 11:39:20 -0600

    tarea de razor

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Pelicula == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies.
+                if (context.Pelicula.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Pelicula.AddRange(
+                    new Pelicula
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                          Rating = "R"
+                    },
+
+                    new Pelicula
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "R"
+                    },
+
+                    new Pelicula
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+
+                    new Pelicula
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
